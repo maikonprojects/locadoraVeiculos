6d5a1c46e66dd174485505c592ebee64ab0b6ebc
maikonprojects <162837965+maikonprojects@users.noreply.github.com>
2025-05-08 10:08:44 -0300

    subindo projeto

---
+++ locadoraVeiculo/src/main/java/com/desafio/locadoraVeiculo/LocadoraVeiculoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.locadoraVeiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraVeiculoApplication.class, args);
+	}
+
+}
---
+++ locadoraVeiculo/src/test/java/com/desafio/locadoraVeiculo/LocadoraVeiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.locadoraVeiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraVeiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
